9d3a93b82a3059d8feefee9cf457ada52170e436
mmkaram <mahdykaram@outlook.com>
2024-01-22 09:06:11 -0800

    added bit system

--- src/chessGame.h
+++ src/chessGame.h
@@ -1,32 +1,66 @@
-#include <string>
+#include <iostream>
 
-#include "chessBoard.h"
-
-class Chess
+class ChessBoard
 {
+public:
+    // Constants for color
+    static const int NULL_COLOR = 0b00;
+    static const int WHITE_COLOR = 0b01;
+    static const int BLACK_COLOR = 0b11;
+
+    // Constants for piece types
+    static const int NULL_TYPE = 0b0000;
+
+    static const int PAWN = 0b0001;
+    static const int KNIGHT = 0b0010;
+    static const int BISHOP = 0b0011;
+    static const int ROOK = 0b0100;
+    static const int QUEEN = 0b0101;
+    static const int KING = 0b0111;
+
 private:
-    Board *board;
+    int turn = WHITE_COLOR;
+    int board[8][8];
 
 public:
-    Chess()
+    // Constructor
+    ChessBoard()
     {
-        board = new Board();
+        // Initialize the board with NULL pieces
+        for (int i = 0; i < 8; ++i)
+        {
+            for (int j = 0; j < 8; ++j)
+            {
+                board[i][j] = createPiece(NULL_COLOR, NULL_TYPE);
+            }
+        }
     }
-    Chess(std::string FEN)
+
+    // Function to set a piece on the board
+    void setPiece(int row, int col, int color, int type)
     {
-        board = new Board(FEN);
+        board[row][col] = createPiece(color, type);
     }
-    ~Chess()
+
+    // Function to get a piece from the board
+    int getPiece(int row, int col) const
     {
-        delete board;
-        board = nullptr;
+        return board[row][col];
     }
-    std::string getBoard()
+
+    // Function to create a piece value
+    static int createPiece(int color, int type)
     {
-        return board->toString();
+        return (color << 6) | type;
     }
-    getMoves()
+
+    int getTurn() const
     {
-        return generateMoves(this->board, COLOR::WHITE);
+        return turn;
     }
+};
+
+class moveGen
+{
+private:
 };
